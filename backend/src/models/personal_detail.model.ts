b39d4d984c61d03b4eec945c22921d04ec81b038
kody <w.jamesdevera@gmail.com>
2025-01-07 17:20:57 +0800

    create personal_detail.model.ts

---
+++ backend/src/models/personal_detail.model.ts
@@ -0,0 +1,63 @@
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes,
+} from "sequelize";
+import sequelize from "../config/db";
+
+class PersonalDetail extends Model<
+  InferAttributes<PersonalDetail>,
+  InferCreationAttributes<PersonalDetail>
+> {
+  declare personalDetailId: CreationOptional<string>;
+  declare firstName: string;
+  declare lastName: string;
+  declare email: string;
+  declare phoneNumber: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+PersonalDetail.init(
+  {
+    personalDetailId: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING((length = 150)),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING((length = 150)),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING((length = 150)),
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING((length = 15)),
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    tableName: "user_accounts",
+    timestamps: true,
+    underscored: true,
+    sequelize,
+  }
+);
+
+(async () => {
+  try {
+    await PersonalDetail.sync({ force: true });
+  } catch (error) {
+    console.error(`Unable to connect to the database:`, error);
+  }
+})();
+
+export default PersonalDetail;
