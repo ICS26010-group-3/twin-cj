a7eef490d13f56e2f13081cdf72302b670f07c20
Artorisity <ralphkylelabaguis@gmail.com>
2025-02-03 13:18:15 +0800

    Allowed the viewing of the page through the "http://localhost:3000/booking_status" path

---
+++ frontend/app/booking_status/page.tsx
@@ -0,0 +1,20 @@
+import BookingStatusDetails from "../components/BookingStatusDetails";
+import BookingStatusIntro from "../components/BookingStatusIntro";
+import BookingStatusPrintButton from "../components/BookingStatusPrintButton";
+import BookingStatusReference from "../components/BookingStatusReference";
+import BookingStatusDetailsReupload from "../components/BookingStatusReupload";
+import HeroImage from "../components/HeroImage";
+import styles from "../page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <HeroImage imageURL="View Booking Status Hero.png" />
+      <BookingStatusIntro />
+      <BookingStatusReference />
+      <BookingStatusDetails headingTitle="You’re officially booked!" headingDescription="Thank you for choosing us for your stay! Below are your booking details:" status="Approved Downpayment" statusColor="green" />
+      <BookingStatusPrintButton />
+      <BookingStatusDetailsReupload />
+    </div>
+  );
+}
